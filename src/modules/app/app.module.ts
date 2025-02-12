ba371ec7c4b8c9b5a9c5e030fa675186cf2c286e
thatcatfromspace <encyclopedia748@gmail.com>
2025-02-12 19:54:19 +0530

    feat: add throttling

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoggerModule } from '../logger/logger.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { LoggerMiddleware } from '../../middlewares/logger.middleware';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -20,9 +21,31 @@ import { LoggerMiddleware } from '../../middlewares/logger.middleware';
     UserModule,
     PostModule,
     ConfigModule.forRoot({ isGlobal: true, load: [() => GLOBAL_CONFIG] }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 10,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 15,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 30,
+      },
+      {
+        name: 'very-long',
+        ttl: 1800000,
+        limit: 200,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: 'APP_GUARD', useClass: ThrottlerGuard }],
   exports: [],
 })
 export class AppModule {
